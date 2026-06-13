0f7b72910ef3bffb2426f571f6e43c2e2c8c2d85
remyz13 <kiwiartz13@gmail.com>
2026-06-13 01:17:34 -0500

    title edits

--- src/components/art-gallery.tsx
+++ src/components/art-gallery.tsx
@@ -56,7 +56,7 @@ export default function ArtGallery({ folder, subfolder }: ArtGalleryProps) {
                         className="group relative mb-4 break-inside-avoid cursor-pointer"
                         onClick={() => setSelected(image)}
                     >
-                        <img src={image.src} className="w-full h-auto block" loading="lazy" />
+                        <img src={image.src} className="w-full h-auto block" />
                         <div className="absolute bottom-0 left-0 w-full bg-[#00000070] text-white px-2 py-1 opacity-0 group-hover:opacity-100 transition-opacity">
                             {image.name}
                         </div>
